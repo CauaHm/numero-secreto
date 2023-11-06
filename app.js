2d762da1ab8eda7b2b8aeee4e78fac17c5ad8a1e
Cauã Henrique_MA <cauamh321@gmail.com>
2023-11-06 19:34:03 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-let num = parseInt(Math.random() *10 + 1);
+alert('Bem vindo ao jogo do número secreto!')
 
-console.log(num);
+let maxNum = 1000;
+let secretNumber = parseInt(Math.random() *maxNum + 1);
+console.log(secretNumber);
+let chute;
+let tentativas = 0;
+
+while (chute != secretNumber) {
+    chute = prompt(`Digite um número de 1 a ${maxNum}`)
+    if (chute > secretNumber) {
+        alert(`O número secreto é menor que o ${chute}`)
+    } else {
+        if (chute < secretNumber) {
+            alert(`O número secreto é maior que o ${chute}`)
+        }
+        tentativas++
+    }
+}
+
+let palavraTentativas = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`Isso ae! Você acertou o número secreto ${secretNumber} com ${tentativas} ${palavraTentativas}`)
