ee1b0242d192507d8ad089a55e8811483679404d
Cauã Henrique_MA <cauamh321@gmail.com>
2023-11-06 13:16:27 -0300

    Adicionando coisas

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ console.log(`O número chutado é ${chute}`);
 
 // Enquanto o chute não for igual ao número secreto 
 while (chute != numSecret) {
-     chute = prompt('Escolha um número entre 0 e 10');
+    chute = prompt('Escolha um número entre 0 e 10');
 
     // Se o chute for igual ao número secreto
     if (chute == numSecret) {
-        alert(`Isso ae! você descobriu o número secreto ${numSecret}`);
+        break;
     } else {
         if (chute > numSecret) {
             alert(`O número secreto é menor do que o ${chute}`);
@@ -26,7 +26,7 @@ while (chute != numSecret) {
     }
 }
 if (tentativas > 1) {
-    alert(`Você acertou o número secreto com ${tentativas} tentativas`)
+    alert(`Isso ae! você acertou o número secreto com ${tentativas} tentativas`)
 } else {
-    alert(`Você acertou o número secreto com ${tentativas} tentativa`)
+    alert(`Isso ae! você acertou o número secreto com ${tentativas} tentativa`)
 }
